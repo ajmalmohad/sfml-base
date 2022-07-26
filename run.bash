ccd7331b30cd95d546f08163d8db9badcc55e436
ajmalmohad <ajmalajmal.2016@gmail.com>
2022-07-26 21:13:08 +0530

    Bash Script to Compile and Run

--- run.bash
+++ run.bash
@@ -1,2 +1,5 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=./sfml && ./sfml-app
+export LD_LIBRARY_PATH=./sfml
+g++ -c main.cpp
+g++ main.o -o main -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio -lsfml-network
+./main
